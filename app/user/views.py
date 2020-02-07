a9f5d22e27ab6b905bcbe93c4840a22cd91766d6
Sonia P <pawlaksonia@gmail.com>
2020-02-07 22:53:41 +0100

    Added manage user endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -18,3 +18,18 @@ class CreateTokenView(ObtainAuthToken):
 # If you authenticate using username and login (Django default user model)
 # You can just include ObtainAuthToken view directly to URLs
 # We need to extend it
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
+# When the get object is called the request will have the user attached to it
+# because of the authentication classes
+# authentication class takes care of getting the authenticated user
+# and assigning it to request
